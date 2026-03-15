b7f930a760b5f2e3c7edbd83caf7780673e172ce
Sadekur <shadekur.rahman60@gmail.com>
2026-03-15 12:16:34 +0600

    Update: layout

---
+++ src/app/(site)/donate/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h2 className="text-3xl text-white font-arial">Welcome to the Donate Page</h2>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/admin/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const layout = () => {
+  return (
+    <div>
+        <h2 className="text-3xl text-white font-arial">Welcome to the Admin Layout</h2>
+    </div>
+  )
+}
+
+export default layout
