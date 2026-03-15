0f4b996d612a9fb9162488e079de529e7b6fb4c9
Sadekur <shadekur.rahman60@gmail.com>
2026-03-15 11:13:22 +0600

    initial commit for human-concern

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-amber-700 flex min-h-screen items-center justify-center">
+      <h2 className="text-3xl text-white font-arial">Welcome to the Home Page</h2>
+    </div>
+  );
+}
