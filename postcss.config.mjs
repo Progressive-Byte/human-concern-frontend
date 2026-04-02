9a0bdb35169aa85b0173e5e8a48bb474744067c7
Sadekur <shadekur.rahman60@gmail.com>
2026-04-02 09:30:50 +0600

    Fix: Top banner adjustment issue

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,6 +2,13 @@ const config = {
   plugins: {
     "@tailwindcss/postcss": {},
   },
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(103.99deg, #403DCE 5.42%, #201F68 83.13%)',
+      },
+    },
+  }
 };
 
 export default config;
--- src/components/layout/Topnoticebar.jsx
+++ src/components/layout/Topnoticebar.jsx
@@ -9,7 +9,7 @@ export default function TopNoticeBar() {
   if (pathname !== "/") return null;
 
   return (
-    <div className="relative w-full h-[52px] z-40 overflow-hidden">
+    <div className="relative w-full h-[79px] z-40 overflow-hidden">
       {/* Background Image */}
       <div
         className="absolute inset-0 bg-[url('/images/topbarNotice.png')] bg-cover bg-center"
@@ -19,8 +19,8 @@ export default function TopNoticeBar() {
       {/* Content */}
       <div className="relative h-full flex items-center justify-center">
         <div className="flex gap-12 whitespace-nowrap text-white font-semibold text-sm md:text-base">
-          <h2>Ramadan Food Campaign 2026</h2>
-          <h2>Iftar & Sohoor Campaign 2026</h2>
+          <h2 className="backdrop-blur-[100px] bg-[linear-gradient(103.99deg,_#403DCE_5.42%,_#201F68_83.13%)] px-[18px] py-[9px] rounded-2xl">Ramadan Food Campaign 2026</h2>
+          <h2 className="backdrop-blur-[100px] bg-[linear-gradient(103.99deg,_#403DCE_5.42%,_#201F68_83.13%)] px-[18px] py-[9px] rounded-2xl">Iftar & Sohoor Campaign 2026</h2>
         </div>
       </div>
     </div>
