12d03aca55ecc0b0ae7accdbcc6d58c2e49b8578
Sadekur <shadekur.rahman60@gmail.com>
2026-04-05 14:25:11 +0600

    feat: add VideoModal component for video playback functionality

--- src/app/(site)/page.jsx
+++ src/app/(site)/page.jsx
@@ -222,7 +222,7 @@ export default function HomePage() {
     <>
       <main className="overflow-hidden text-white">
         {/* ── Hero ── */}
-        <section className="relative w-full h-[1200px] overflow-hidden">
+        <section className="relative w-full lg:h-[1200px] md:h-[750px] h-[600px] overflow-hidden">
           <div
             className="absolute inset-0 bg-[url('/images/hero.png')] bg-center bg-cover bg-no-repeat"
             aria-hidden="true"
@@ -235,12 +235,12 @@ export default function HomePage() {
 
           {/* ── Frosted glass card — */}
           <div className="absolute inset-0 flex items-center">
-            <div className="w-full max-w-[1611px] mt-[-200px] mx-auto">
-              <div className="w-full max-w-[652px] bg-[#FFFFFFB2] backdrop-blur-[100px] px-[77px] py-[60px] rounded-3xl">
-                <h1 className="text-[56px] leading-[1.1] font-semibold text-[#383838] [text-shadow:0px_4px_25px_rgba(255,255,255,0.25)]">
+            <div className="w-full max-w-[1611px] lg:mt-[-200px] mt-[200px] mx-auto px-6 lg:px-8">
+              <div className="w-full lg:max-w-[652px] bg-[#FFFFFFB2] backdrop-blur-[100px] px-[20px] py-[30px] md:px-[77px] md:py-[60px] rounded-3xl">
+                <h1 className="md:text-[56px] text-3xl md:leading-[1.1] font-semibold text-[#383838] [text-shadow:0px_4px_25px_rgba(255,255,255,0.25)]">
                   Give with <span className="font-bold font-playfair italic">Purpose. Transform</span> lives.
                 </h1>
-                <p className="text-[#383838] font-medium py-7 text-2xl [text-shadow:0px_0px_3px_rgba(255,255,255,1)]">
+                <p className="text-[#383838] font-medium py-5 md:py-7 text-md md:text-2xl [text-shadow:0px_0px_3px_rgba(255,255,255,1)]">
                   Your trusted platform for Zakat, Sadaqah, and humanitarian giving.
                 </p>
                 <div className="flex items-center gap-3 flex-wrap mb-7">
@@ -264,7 +264,7 @@ export default function HomePage() {
                     (label) => (
                       <span
                         key={label}
-                        className="flex items-center gap-1.5 text-[14px] text-[#383838] font-medium"
+                        className="flex items-center gap-1.5 text-[12px] md:text-[14px] text-[#383838] font-medium"
                       >
                         <span className="">
                           {CheckIcon}
