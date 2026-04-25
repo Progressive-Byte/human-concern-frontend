d2b0d0fc53823a1130d03968fed6da960fefee3b
Sadekur <shadekur.rahman60@gmail.com>
2026-04-25 14:35:10 +0600

    Update: getCurrentUser api comment

--- src/services/authService.js
+++ src/services/authService.js
@@ -8,9 +8,9 @@ export function register(payload) {
   return apiRequest("auth/register", { method: "POST", body: JSON.stringify(payload) });
 }
 
-export function getCurrentUser() {
-  return apiRequest("auth/me");
-}
+// export function getCurrentUser() {
+//   return apiRequest("auth/me");
+// }
 
 export function changePassword(payload) {
   return apiRequest("auth/change-password", { method: "POST", body: JSON.stringify(payload) });
