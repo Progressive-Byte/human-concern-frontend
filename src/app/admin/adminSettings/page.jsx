728216427ff25daae1bce6d23cf61b9fd7d233b3
Sadekur <shadekur.rahman60@gmail.com>
2026-04-25 15:35:27 +0600

    Update: Admin setting component naming

--- src/app/admin/adminSettings/page.jsx
+++ src/app/admin/adminSettings/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h2 className="text-3xl text-white font-arial">Welcome to the Admin Users Page</h2>
+      <h2 className="text-3xl text-white font-arial">Welcome to the Admin Settings Page</h2>
     </div>
   )
 }
