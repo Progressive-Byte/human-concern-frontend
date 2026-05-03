328206688802e8a123c16e712ddf95fad8097282
Sadekur <shadekur.rahman60@gmail.com>
2026-05-03 11:00:57 +0600

    Remove: unnecessery comment

--- src/app/donate/steps/Step7PaymentDetails.jsx
+++ src/app/donate/steps/Step7PaymentDetails.jsx
@@ -65,11 +65,6 @@ const Step7PaymentDetails = () => {
       onPrev={() => handlePrev(6)}
       prevLabel="Payment Details"
       nextLabel="Complete Donation"
-      nextIcon={
-        <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-        </svg>
-      }
     >
       <div className="flex flex-col gap-5">
         <div className="border border-[#E5E5E5] rounded-2xl bg-white overflow-hidden px-4">
