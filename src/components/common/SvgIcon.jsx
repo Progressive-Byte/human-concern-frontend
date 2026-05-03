dfd5ef299399c1648bc44805650276dccb91be36
Sadekur <shadekur.rahman60@gmail.com>
2026-05-03 16:56:48 +0600

    Update: svg file alignment

--- src/components/common/SvgIcon.jsx
+++ src/components/common/SvgIcon.jsx
@@ -260,13 +260,12 @@ export const pieChartIcon = (
     <path d="M17.6747 13.2417C17.1445 14.4955 16.3153 15.6002 15.2596 16.4595C14.2038 17.3187 12.9537 17.9063 11.6184 18.1707C10.2831 18.4352 8.90337 18.3685 7.59981 17.9766C6.29624 17.5846 5.10854 16.8793 4.14053 15.9223C3.17253 14.9653 2.4537 13.7857 2.04688 12.4867C1.64006 11.1877 1.55765 9.8088 1.80685 8.47059C2.05605 7.13238 2.62926 5.87559 3.47639 4.81009C4.32351 3.74459 5.41875 2.90283 6.66635 2.3584" stroke="#737373" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>
 )
-
-  export const cardIcon = (
-    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M16.666 4.1665H3.33268C2.41221 4.1665 1.66602 4.9127 1.66602 5.83317V14.1665C1.66602 15.087 2.41221 15.8332 3.33268 15.8332H16.666C17.5865 15.8332 18.3327 15.087 18.3327 14.1665V5.83317C18.3327 4.9127 17.5865 4.1665 16.666 4.1665Z" stroke="#737373" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-      <path d="M1.66602 8.3335H18.3327" stroke="#737373" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
-    </svg>
-  )
+export const cardIcon = (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M16.666 4.1665H3.33268C2.41221 4.1665 1.66602 4.9127 1.66602 5.83317V14.1665C1.66602 15.087 2.41221 15.8332 3.33268 15.8332H16.666C17.5865 15.8332 18.3327 15.087 18.3327 14.1665V5.83317C18.3327 4.9127 17.5865 4.1665 16.666 4.1665Z" stroke="#737373" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1.66602 8.3335H18.3327" stroke="#737373" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+)
 
   export const UserDashboardIcon = (
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
