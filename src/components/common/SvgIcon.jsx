472105a6fed73938b2b2a7cb36233dd9804f5c03
Sadekur <shadekur.rahman60@gmail.com>
2026-05-03 16:55:48 +0600

    Update: svg file alignment

--- src/components/common/SvgIcon.jsx
+++ src/components/common/SvgIcon.jsx
@@ -209,13 +209,12 @@ export const ArrowNextIcon = (
     <path d="M8 3.33398L12.6667 8.00065L8 12.6673" stroke="white" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
   </svg>
 )
-
-  export const ArrowPrevIcon = (
-    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M7.99967 12.6673L3.33301 8.00065L7.99967 3.33398" stroke="#383838" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-      <path d="M12.6663 8H3.33301" stroke="#383838" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
-    </svg>
-  )
+export const ArrowPrevIcon = (
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7.99967 12.6673L3.33301 8.00065L7.99967 3.33398" stroke="#383838" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M12.6663 8H3.33301" stroke="#383838" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+)
 
   export const NoticeIcon = (
     <svg width="12" height="15" viewBox="0 0 12 15" fill="none" xmlns="http://www.w3.org/2000/svg">
