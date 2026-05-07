a3894f9b34d76993a8079dfba6e88e14da51ef97
Sadekur <shadekur.rahman60@gmail.com>
2026-05-07 16:45:09 +0600

    Update: Recurring Schedules screen

--- src/app/dashboard/schedules/page.jsx
+++ src/app/dashboard/schedules/page.jsx
@@ -1,4 +1,4 @@
-import { CalendarIcon, ClockIcon, PauseIcon, PlayIcon } from "@/components/common/SvgIcon";
+import { CalendarIcon, ClockIcon, EditIcon, PauseIcon, PlayIcon } from "@/components/common/SvgIcon";
 import DashboardHeader from "../components/DashboardHeader";
 
 const schedules = [
@@ -38,13 +38,6 @@ const schedules = [
 ];
 
 const frequencyLabel = { Weekly: "per week", Monthly: "per month", Daily: "per day" };
-
-const EditIcon = (
-  <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
-  </svg>
-);
 
 const TrashIcon = (
   <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
