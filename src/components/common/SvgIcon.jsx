bafc661c2ab75889142c225de7162d62121858b7
Sadekur <shadekur.rahman60@gmail.com>
2026-05-09 14:38:50 +0600

    Saperate: svg from main component

--- src/components/common/SvgIcon.jsx
+++ src/components/common/SvgIcon.jsx
@@ -465,3 +465,8 @@ export const CloseModal = (
     <line x1="6" y1="6" x2="18" y2="18" />
   </svg>
 )
+export const BellIcon = (
+  <svg width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" /><path d="M13.73 21a2 2 0 0 1-3.46 0" />
+  </svg>
+);
