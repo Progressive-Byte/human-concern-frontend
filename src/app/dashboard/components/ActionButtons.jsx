14c340dab970030ad4eaf4820941c8657963f865
Sadekur <shadekur.rahman60@gmail.com>
2026-05-09 16:11:47 +0600

    add a slug field to the data, rename the route folder from [id] to [slug], update the   detail page to find by slug, and update ActionButtons to use slug

--- src/app/dashboard/components/ActionButtons.jsx
+++ src/app/dashboard/components/ActionButtons.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { EyeIcon, PauseIcon, PlayIcon, TrashIcon } from "@/components/common/SvgIcon";
 
-const ActionButtons = ({ isActive, id }) => {
+const ActionButtons = ({ isActive, slug }) => {
   return (
     <>
       <button
