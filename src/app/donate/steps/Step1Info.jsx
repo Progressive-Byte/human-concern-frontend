4479482549a98abcafd0fb85a2ff6ff021fd66b9
Sadekur <shadekur.rahman60@gmail.com>
2026-05-11 11:23:55 +0600

    Update: extract ISO3_TO_ISO2, resolveCountryIso, and resolveStateIso into   src/utils/isoHelpers.js and import them back.

--- src/app/donate/steps/Step1Info.jsx
+++ src/app/donate/steps/Step1Info.jsx
@@ -10,36 +10,7 @@ import StepLayout from "../DonateComponents/StepLayout";
 import Field from "@/components/ui/Field";
 import CustomDropdown from "@/components/common/CustomDropdown";
 import { ChevronIcon } from "@/components/common/SvgIcon";
-
-const ISO3_TO_ISO2 = {
-  USA: "US", GBR: "GB", CAN: "CA", AUS: "AU", ARE: "AE",
-  IND: "IN", PAK: "PK", BGD: "BD", NZL: "NZ", DEU: "DE",
-  FRA: "FR", SAU: "SA", QAT: "QA", KWT: "KW", TUR: "TR",
-};
-
-function resolveCountryIso(value) {
-  if (!value) return null;
-  const all = Country.getAllCountries();
-  const upper = value.toUpperCase();
-  return (
-    all.find((c) => c.name === value)?.isoCode ||
-    all.find((c) => c.isoCode === upper)?.isoCode ||
-    all.find((c) => c.isoCode === ISO3_TO_ISO2[upper])?.isoCode ||
-    all.find((c) => c.name.toLowerCase() === value.toLowerCase())?.isoCode ||
-    null
-  );
-}
-
-function resolveStateIso(stateName, countryIso) {
-  if (!stateName || !countryIso) return null;
-  const states = State.getStatesOfCountry(countryIso);
-  return (
-    states.find((s) => s.name === stateName)?.isoCode ||
-    states.find((s) => s.isoCode === stateName.toUpperCase())?.isoCode ||
-    states.find((s) => s.name.toLowerCase() === stateName.toLowerCase())?.isoCode ||
-    null
-  );
-}
+import { resolveCountryIso, resolveStateIso } from "@/utils/isoHelpers";
 
 const Step1Info = ({ campaignSlug }) => {
   const { data, update } = useDonation();
