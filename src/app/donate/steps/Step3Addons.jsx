3318a6405e4a72c369b086d9a67442b326b4bfe6
Sadekur <shadekur.rahman60@gmail.com>
2026-05-17 15:16:58 +0600

    Update: import generateDatesInRange function for date handling in Step3Addons

--- src/app/donate/steps/Step3Addons.jsx
+++ src/app/donate/steps/Step3Addons.jsx
@@ -6,6 +6,7 @@ import { useDonation } from "@/context/DonationContext";
 import { useStepNavigation } from "@/hooks/useStepNavigation";
 import StepLayout from "./StepComponents/StepLayout";
 import { apiRequest } from "@/services/api";
+import { generateDatesInRange } from "./StepComponents/countOccurrences";
 import AddOnsList from "./StepComponents/Step3components/AddOnsList";
 import TippingSection from "./StepComponents/Step3components/TippingSection";
 import PaymentGatewaySelector from "./StepComponents/Step3components/PaymentGatewaySelector";
