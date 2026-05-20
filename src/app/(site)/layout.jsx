ea30da1ce99d7dba6fab73fdc0585602d7e41a68
Sadekur <shadekur.rahman60@gmail.com>
2026-05-20 12:16:48 +0600

    refactor: change SiteLayout function declaration to arrow function syntax

--- src/app/(site)/layout.jsx
+++ src/app/(site)/layout.jsx
@@ -8,7 +8,7 @@ export const metadata = {
     "Connect with verified campaigns and make a real difference. Donate to education, healthcare, clean water, and more.",
 };
 
-export default function SiteLayout({ children }) {
+const SiteLayout = ({ children }) => {
   return (
     <>
       <div className="fixed top-0 left-0 right-0 z-50 flex flex-col">
